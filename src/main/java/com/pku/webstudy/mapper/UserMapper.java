ae69d8fcc445fa7e4c2268722880726c87c79f80
GaoYaoKun <gyk2009@126.com>
2020-03-14 22:18:31 +0800

    增加cookies，保持用户登录态

--- src/main/java/com/pku/webstudy/mapper/UserMapper.java
+++ src/main/java/com/pku/webstudy/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.pku.webstudy.mapper;
 import com.pku.webstudy.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,4 +16,7 @@ import org.springframework.stereotype.Component;
 public interface UserMapper {
     @Insert("INSERT INTO user (name, account_id, token, gmt_create, gmt_modify) values (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModify})")
     void insert(User user);
+
+    @Select("SELECT * FROM user WHERE token = #{token}")
+    User findByToken(@Param("token") String token); //参数不是类时，需要@param注解
 }
